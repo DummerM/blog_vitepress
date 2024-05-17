53ed3bc23bbad7669863077a21ebe94f44b67e25
XinFangFang <550212808@qq.com>
2024-05-17 10:34:07 +0800

    首页改造

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,17 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
- 
+import Layout from "./Layout.vue"
+
 export default {
-  ...DefaultTheme,
-  NotFound: () => "404", // <- this is a Vue 3 functional component
-  enhanceApp({ app, router, siteData }) {
-    // app is the Vue 3 app instance from createApp()
-    // router is VitePress' custom router (see `lib/app/router.js`)
-    // siteData is a ref of current site-level metadata.
-  },
+    extends: DefaultTheme,
+    // 使用注入插槽的包装组件覆盖 Layout
+    Layout: Layout,
+    NotFound: () => "404", // <- this is a Vue 3 functional component
+    enhanceApp({ app, router, siteData }) {
+        // app is the Vue 3 app instance from createApp()
+        // router is VitePress' custom router (see `lib/app/router.js`)
+        // siteData is a ref of current site-level metadata.
+    },
 };
+
+
--- docs/index.md
+++ docs/index.md
@@ -3,19 +3,19 @@
 layout: home
 
 hero:
-  name: "xingfangfang"
-  text: "bolg"
+  name: "xingfangfangのblog"
+  text: " "
   image: 
     src: "/imgs/bg.svg"
     style: "height: 100vh;width: 100vw;maxHeight: 100vh !important;maxWidth: 100vw !important;"
   tagline: If I sleep now, I will dream — but if I study now, I will make my dreams come true.
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 前端
+      link: /front/index
+    # - theme: alt
+    #   text: API Examples
+    #   link: /api-examples
 
 features:
   - title: Feature A
